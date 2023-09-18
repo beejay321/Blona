6946422fec8cb9d1c0b23ffe21aa3d298f985b35
Oluwabusola <jibodu.busola@gmail.com>
2023-09-18 11:31:48 +0200

    modified mongo connection

--- app/lib/mongo.ts
+++ app/lib/mongo.ts
@@ -1,21 +1,38 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URL!);
+
+export async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+export default connectDB;
+
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    mongoose.connect(process.env.MONGO_URL!);
+// const connectDB = async () => {
+//   try {
+//     console.log(process.env.MONGO_URL);
+//     mongoose.connect(process.env.MONGO_URL!);
 
-    const connection = mongoose.connection;
-    connection.on("connected", () => {
-      console.log("Connected to mongo");
-      console.table(["Connected", "to", "Mongo"]);
-    });
+//     const connection = mongoose.connection;
+//     connection.on("connected", () => {
+//       console.log("Connected to mongo");
+//       console.table(["Connected", "to", "Mongo"]);
+//     });
 
-    connection.on("error", (err) => {
-      console.log("Error connecting to MongoDB connection error. " + err);
-      process.exit();
-    });
-  } catch (error) {
-    console.log("There was an error connecting to the DB", error);
-  }
-};
-export default connectDB;
+//     connection.on("error", (err) => {
+//       console.log("Error connecting to MongoDB connection error. " + err);
+//       process.exit();
+//     });
+//   } catch (error) {
+//     console.log("There was an error connecting to the DB", error);
+//   }
+// };
+// export default connectDB;
